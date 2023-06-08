3066a89abca4d1bcc6f4cdde4a79281337fefcdd
Hoang Minh Thanh <hmthanhgm@gmail.com>
2023-06-08 16:19:04 +0700

    update all

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -3,6 +3,7 @@ import Form from "@/components/form";
 import Link from "next/link";
 
 export default function Login() {
+  
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-gray-50">
       <div className="z-10 w-full max-w-md overflow-hidden rounded-2xl border border-gray-100 shadow-xl">
---
+++ lib/createData.ts
@@ -0,0 +1,33 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+async function createData() {
+  try {
+    const newData = await prisma.HTTPStatusCode.create({
+      data: {
+        server_id: 3,
+        app_id: 4,
+        servername: "VNPT",
+        appname: "Baomoi",
+        Total200: 9000000,
+        Total301: 9000000,
+        Total302: 9000000,
+        Total304: 9000000,
+        Total400: 9000000,
+        Total403: 9000000,
+        Total404: 9000000,
+        Total500: 9000000,
+        Total502: 9000000,
+        TotalClose: 90000000,
+        TotalUnknown: 90000000,
+      },
+    });
+    console.log("Data created:", newData);
+  } catch (error) {
+    console.error("Error creating data:", error);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+export default createData;
